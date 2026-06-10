98ef37dc3310c1c4bc5a9dab438bbe369188217e
jaikaran109 <jaik05581@gmail.com>
2026-06-11 00:50:05 +0530

    Create LC_Q1252_CellsWithOddValuesInMatrix.java

---
+++ Arrays/Ques/Solution/Easy/LC_Q1252_CellsWithOddValuesInMatrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+
+        for (int[] idx : indices) {
+            rows[idx[0]]++;
+            cols[idx[1]]++;
+        }
+
+        int odd = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((rows[i] + cols[j]) % 2 != 0) {
+                    odd++;
+                }
+            }
+        }
+
+        return odd;
+    }
+}
