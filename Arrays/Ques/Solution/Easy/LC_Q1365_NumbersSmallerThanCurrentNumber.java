44dbb0f71881ddadd19a8fdebf9286249896e970
jaikaran109 <jaik05581@gmail.com>
2026-06-10 21:44:09 +0530

    Create LC_Q1365_NumbersSmallerThanCurrentNumber.java

---
+++ Arrays/Ques/Solution/Easy/LC_Q1365_NumbersSmallerThanCurrentNumber.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] freq = new int[101];
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        for(int i = 1 ; i <=100 ; i++){
+            freq[i] += freq[i - 1];
+        }
+
+        int[] ans = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == 0){
+                ans[i] = 0;
+            }else{
+                ans[i] = freq[nums[i] - 1];
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+// Brute Force
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+//         int[] ans = new int[nums.length];
+//         for(int i = 0 ; i < nums.length ; i++){
+//             int count = 0;
+//             for(int j = 0 ; j < nums.length ; j++){
+//                 if(nums[i] > nums[j]) count++;
+//             }
+//             ans[i] = count;
+//         }
+//         return ans;
+//     }
+// }
