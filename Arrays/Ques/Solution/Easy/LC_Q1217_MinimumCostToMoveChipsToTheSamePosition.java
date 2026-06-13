4c03a2c4344b125a738573a25c34a442ab4fcbf5
jaikaran109 <jaik05581@gmail.com>
2026-06-14 02:56:55 +0530

    Create LC_Q1217_MinimumCostToMoveChipsToTheSamePosition.java

---
+++ Arrays/Ques/Solution/Easy/LC_Q1217_MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+        for(int num : position){
+            if(num % 2 == 0) even++;
+            else odd++;
+        }
+        return Math.min(odd,even);
+    }
+}
