cf86a67012bffdcf6ca713b8382a02ce80d22e8d
jaikaran109 <jaik05581@gmail.com>
2026-07-04 16:01:14 +0530

    Create LC_Q287_FindDuplicateNumber.java

---
+++ 06-Searching/Medium/LC_Q287_FindDuplicateNumber.java
@@ -0,0 +1,37 @@
+// More optimal - use 2 pointer
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        boolean[] isFound = new boolean[nums.length + 1];
+
+        for(int num : nums){
+            if(isFound[num]) return num;
+            isFound[num] = true;
+        }
+        return -1;
+    }
+}
+
+
+
+
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+
+//         for(int i = 0 ; i < nums.length ; i++){
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+//             if(map.get(nums[i]) > 1) return nums[i];
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+// Arrays.sort(nums);
+// for(int i = 0 ; i < nums.length - 1; i++){
+//     if(nums[i] == nums[i+1]) return nums[i];
+// }
