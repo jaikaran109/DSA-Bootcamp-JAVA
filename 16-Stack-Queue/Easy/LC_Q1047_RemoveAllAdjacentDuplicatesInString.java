f6f7dde225b4ea166448088fe9e9ec3329058700
jaikaran109 <jaik05581@gmail.com>
2026-07-12 01:50:31 +0530

    Update LC_Q1047_RemoveAllAdjacentDuplicatesInString.java

--- 16-Stack-Queue/Easy/LC_Q1047_RemoveAllAdjacentDuplicatesInString.java
+++ 16-Stack-Queue/Easy/LC_Q1047_RemoveAllAdjacentDuplicatesInString.java
@@ -1,16 +1,36 @@
 class Solution {
     public String removeDuplicates(String s) {
         Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(!st.isEmpty() && st.peek() == c){
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
 
-        for(char ch : s.toCharArray()){
-            if(!st.isEmpty() && st.peek() == ch) st.pop();
-            else st.push(ch);
+        StringBuilder res = new StringBuilder();
+        for(char c : st) res.append(c);
+
+        return res.toString();
         }
-        
-        String ans = "";
-        for(char ch : st){
-            ans += ch;
-        }
-        return ans;
-    }
 }
+
+
+
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         Stack<Character> st = new Stack<>();
+
+//         for(char ch : s.toCharArray()){
+//             if(!st.isEmpty() && st.peek() == ch) st.pop();
+//             else st.push(ch);
+//         }
+        
+//         String ans = "";
+//         for(char ch : st){
+//             ans += ch;
+//         }
+//         return ans;
+//     }
+// }
